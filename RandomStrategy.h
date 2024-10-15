a5aea6cdfcd5057ead40ab03c1a771a9d193d5cc
Shaun Phillips <shaunmp20@gmail.com>
2024-10-15 16:50:29 +1030

    Comment RandomStrategy.h

--- RandomStrategy.h
+++ RandomStrategy.h
@@ -1,10 +1,14 @@
 #ifndef RANDOMSTRATEGY_H
 #define RANDOMSTRATEGY_H
 #include "MarketNeutralStrategy.h"
+//Class relationship
 class RandomStrategy : public MarketNeutralStrategy {
 public:
+    //Constructor
     RandomStrategy() : MarketNeutralStrategy("Random") {}
+    //Execute moving average strategy
     double execute(double currentPrice, const std::string& marketEvent) override {
+        //Calculates action
         double action = (rand() % 3 - 1) * currentPrice * 0.01;
         return action;
     }
