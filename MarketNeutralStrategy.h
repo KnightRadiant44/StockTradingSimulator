0855df76d04c7a612ce13407359b6b66d85f8b21
Shaun Phillips <shaunmp20@gmail.com>
2024-10-15 16:39:22 +1030

    Comment MarketNeutralStrategy.h

--- MarketNeutralStrategy.h
+++ MarketNeutralStrategy.h
@@ -1,10 +1,14 @@
 #ifndef MARKETNEUTRALSTRATEGY_H
 #define MARKETNEUTRALSTRATEGY_H
 #include "TradingStrategy.h"
+//Class relationship
 class MarketNeutralStrategy : public TradingStrategy {
 public:
+    //Constructor initialising strategy name
     MarketNeutralStrategy(const std::string& n) : TradingStrategy(n) {}
+    //Virtual function to execute the strategy
     virtual double execute(double currentPrice, const std::string& marketEvent) override = 0;
+    //Virtual destructor
     virtual ~MarketNeutralStrategy() = default;
 };
-#endif // MARKETNEUTRALSTRATEGY_H
+#endif //MARKETNEUTRALSTRATEGY_H
