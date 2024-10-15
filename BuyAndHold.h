ff1e618a2aca569d9415c3d4650ea4876cec777d
Shaun Phillips <shaunmp20@gmail.com>
2024-10-15 16:19:22 +1030

    Comment BuyAndHold.h

--- BuyAndHold.h
+++ BuyAndHold.h
@@ -1,18 +1,23 @@
 #ifndef BUYANDHOLD_H
 #define BUYANDHOLD_H
 #include "DirectionalStrategy.h"
+//Class relationship
 class BuyAndHoldStrategy : public DirectionalStrategy {
 public:
+    //Constructor initalising strategy name
     BuyAndHoldStrategy() : DirectionalStrategy("Buy and Hold") {}
+    //Executes buy and hold strategy
     double execute(double currentPrice, const std::string& marketEvent) override {
         double action = currentPrice * 0.01;
-        // Adjust action based on market events
+        //Adjust action based on market events
         if (marketEvent.find("bull market") != std::string::npos) {
-            action *= 1.2; // Increase buying during bull market
+            //Increase buying during bull market
+            action *= 1.2;
         } else if (marketEvent.find("market downturn") != std::string::npos) {
-            action *= 0.8; // Decrease buying during market downturn
+             //Decrease buying during market downturn
+            action *= 0.8;
         }
         return action;
     }
 };
-#endif // BUYANDHOLD_H
+#endif //BUYANDHOLD_H
