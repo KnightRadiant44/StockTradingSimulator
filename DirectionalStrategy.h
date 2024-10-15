0914dd2c5e5833ee9aaee295019e91ba729a4d5c
Shaun Phillips <shaunmp20@gmail.com>
2024-10-15 16:20:55 +1030

    Comment DirectionalStrategy.h

--- DirectionalStrategy.h
+++ DirectionalStrategy.h
@@ -1,10 +1,14 @@
 #ifndef DIRECTIONALSTRATEGY_H
 #define DIRECTIONALSTRATEGY_H
 #include "TradingStrategy.h"
+//Class relationship
 class DirectionalStrategy : public TradingStrategy {
 public:
+    //Constructor initialising strategy name
     DirectionalStrategy(const std::string& n) : TradingStrategy(n) {}
+    //Virtual function to execute the strategy
     virtual double execute(double currentPrice, const std::string& marketEvent) override = 0;
+    //Virtual destructor
     virtual ~DirectionalStrategy() = default;
 };
-#endif // DIRECTIONALSTRATEGY_H
+#endif //DIRECTIONALSTRATEGY_H
